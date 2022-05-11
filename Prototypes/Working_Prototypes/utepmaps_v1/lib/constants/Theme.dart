194de514848a7b7258b2a5b3a5a941c23e34ac8c
Noah Padilla <npizza10@gmail.com>
2022-05-10 21:05:37 -0600

    init commit with working drawer

---
+++ Prototypes/Working_Prototypes/utepmaps_v1/lib/constants/Theme.dart
@@ -0,0 +1,25 @@
+import 'dart:ui' show Color;
+
+//utepX_1 = no opacity or full color
+//utepX_2 = half opacity or half faded
+//utepX_3 = full opacity or almost completely faded
+
+class UtepMapsColors {
+  static const Color utepOrange_1 = Color.fromRGBO(255, 130, 0, 1.0);
+
+  static const Color utepOrange_2 = Color.fromRGBO(255, 130, 0, 0.5);
+
+  static const Color utepOrange_3 = Color.fromRGBO(255, 130, 0, 0.1);
+
+  static const Color utepBlue_1 = Color.fromRGBO(4, 30, 66, 1.0);
+
+  static const Color utepBlue_2 = Color.fromRGBO(4, 30, 66, 0.5);
+
+  static const Color utepBlue_3 = Color.fromRGBO(4, 30, 66, 0.1);
+
+  static const Color utepSilver_1 = Color.fromRGBO(177, 179, 179, 1.0);
+
+  static const Color utepSilver_2 = Color.fromRGBO(177, 179, 179, 0.5);
+
+  static const Color utepSilver_3 = Color.fromRGBO(177, 179, 179, 0.1);
+}
---
+++ Prototypes/Working_Prototypes/utepmaps_v1/lib/screens/home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:utepmaps_v1/constants/Theme.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  final String title = "UTEP MAPS";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: UtepMapsColors.utepBlue_1,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Buildings'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Eateries'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Shuttle Stops'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Parking'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
