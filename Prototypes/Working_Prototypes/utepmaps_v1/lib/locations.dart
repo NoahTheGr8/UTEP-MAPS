413a396ede4ff46598b2d394aea5e928c36a970d
Noah Padilla <npizza10@gmail.com>
2022-05-13 01:14:28 -0600

    add the other 2 location array types

--- Prototypes/Working_Prototypes/utepmaps_v1/lib/locations.dart
+++ Prototypes/Working_Prototypes/utepmaps_v1/lib/locations.dart
@@ -1,8 +1,14 @@
-import 'location_types/eatery.dart';
+import 'package:utepmaps_v1/location_types/shuttle_stop.dart';
+
 import 'location_types/building.dart';
+import 'location_types/eatery.dart';
+import 'location_types/shuttle_stop.dart';
+import 'location_types/parking.dart';
 
 //This is the class containing ALL of the locations. Any location can be accessed from this class
 class Locations {
   List<Building> buildings = [];
   List<Eatery> eateries = [];
+  List<ShuttleStop> shuttleStops = [];
+  List<Parking> parking = [];
 }
