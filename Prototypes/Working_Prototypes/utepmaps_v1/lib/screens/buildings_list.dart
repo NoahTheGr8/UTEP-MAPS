b8eca2a9b8b9d3f644dd1b849fa38295ee2647c6
Noah Padilla <npizza10@gmail.com>
2022-05-24 19:17:15 -0600

    add baseline for calculating distance from user

--- Prototypes/Working_Prototypes/utepmaps_v1/lib/screens/buildings_list.dart
+++ Prototypes/Working_Prototypes/utepmaps_v1/lib/screens/buildings_list.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:utepmaps_v1/location_types/building.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:utepmaps_v1/distance_calculator.dart';
+
+import '../location_types/building.dart';
 import '../location_types/locations.dart';
+import '../location_types/location.dart';
 
 class BuildingList extends StatefulWidget {
   static const String route = 'BuildingList';
@@ -20,14 +24,27 @@ class BuildingListState extends State<BuildingList> {
     initializeSelection();
   }
 
-  void initializeSelection() {
+  void initializeSelection() async {
     //TODO - QUERY ALL LOCATIONS FROM DB IN HERE
 
+    // LocationPermission permission = await Geolocator.requestPermission();
+    // Position position = await Geolocator.getCurrentPosition(
+    //     desiredAccuracy: LocationAccuracy.high);
+    // var distCalc = DistanceCalculator(
+    //     position); //calculates all distances of all buildings relative to user before showing a list of them
+
     Building ccsb = Building(
-        "Chemistry and Computer Science", "CCSB", 31.76797, -106.50219);
-    Building bell = Building("Bell Hall", "Bell", 31.76850, -106.50507);
+        "Chemistry and Computer Science",
+        "CCSB",
+        31.76797,
+        -106.50219,
+        0); //Building("Chemistry and Computer Science", "CCSB", 31.76797,-106.50219, distCalc.distFromUser["CCSB"]!);
+    Building bell = Building("Bell Hall", "BELL", 31.76850, -106.50507,
+        0); //Building("Bell Hall", "BELL", 31.76850, -106.50507,distCalc.distFromUser["BELL"]!);
     locations.buildings.add(ccsb);
     locations.buildings.add(bell);
+    print("done creating buildings");
+    print(" dist from user >>>> ${ccsb.distFromUser}");
   }
 
   @override
@@ -65,13 +82,13 @@ class _ListBuilderState extends State<ListBuilder> {
         itemCount: widget.buildingList.length,
         itemBuilder: (context, int index) {
           return ListTile(
-              leading: Icon(Icons.apartment_sharp),
+              leading: const Icon(Icons.apartment_sharp),
               onTap: () => _toggle(
                   index), //TODO update this to navigate to desired location
-              trailing: const Text('dist from user'),
-              title: Text('${widget.buildingList.elementAt(index).name}'),
-              subtitle:
-                  Text('${widget.buildingList.elementAt(index).locAbbrev}'));
+              trailing: Text(
+                  '${widget.buildingList.elementAt(index).distFromUser.toStringAsFixed(2)} miles away'),
+              title: Text(widget.buildingList.elementAt(index).name),
+              subtitle: Text(widget.buildingList.elementAt(index).locAbbrev));
         });
   }
 }
